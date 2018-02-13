6bf15094ea1b8590846386a5194446f98ae0f121
Lukas Rieder <lukas.rieder@iml.unibe.ch>
2018-02-13 10:31:08 +0100

    Follow style guide and emit lifecycle events

--- dist/collection/components/range-test/range-test.js
+++ dist/collection/components/range-test/range-test.js
@@ -2,24 +2,34 @@ export class RangeTest {
     constructor() {
         this.state = { value: 1, min: 0, max: 5 };
     }
-    watchValue(value) { this.changeValue(value); }
-    watchMin(min) { this.changeMin(min); }
-    watchMax(max) { this.changeMax(max); }
+    watchValue(value) {
+        this.changeValue(value);
+    }
+    watchMin(min) {
+        this.changeMin(min);
+    }
+    watchMax(max) {
+        this.changeMax(max);
+    }
     componentWillLoad() {
-        this.host.addEventListener('change', (event) => console.info('[wc:range]', 'emit:change', event['detail']));
+        console.log('[wc:range]', 'The component is about to be rendered');
+        this.lifeCycle.emit('willLoad');
     }
-    render() {
-        return (h("div", null,
-            h("button", { onClick: () => this.decrement(), disabled: this.state.value <= this.state.min }, "Less"),
-            h("span", { class: "value" }, this.state.value),
-            h("button", { onClick: () => this.increment(), disabled: this.state.value >= this.state.max }, "More")));
+    componentDidLoad() {
+        console.log('[wc:range]', 'The component has been rendered');
+        this.lifeCycle.emit('didLoad');
     }
-    changeState(state, notify = false) {
-        console.info('[wc:range]', 'state change', this.state, state);
-        this.state = state;
-        if (notify) {
-            this.change.emit(state.value);
-        }
+    componentWillUpdate() {
+        console.log('[wc:range]', 'The component will update');
+        this.lifeCycle.emit('willUpdate');
+    }
+    componentDidUpdate() {
+        console.log('[wc:range]', 'The component did update');
+        this.lifeCycle.emit('didUpdate');
+    }
+    componentDidUnload() {
+        console.log('[wc:range]', 'The view has been removed from the DOM');
+        this.lifeCycle.emit('didUnload');
     }
     changeValue(value, notify = false) {
         value = Math.min(this.state.max, Math.max(this.state.min, Math.round(value)));
@@ -47,15 +57,29 @@ export class RangeTest {
             });
         }
     }
+    changeState(state, notify = false) {
+        console.info('[wc:range]', 'state change', this.state, state);
+        this.state = state;
+        if (notify) {
+            console.info('[wc:range]', 'emit change', state.value);
+            this.change.emit(state.value);
+        }
+    }
     increment() {
         this.changeValue(this.state.value + 1, true);
     }
     decrement() {
         this.changeValue(this.state.value - 1, true);
     }
+    render() {
+        return (h("div", null,
+            h("button", { onClick: () => this.decrement(), disabled: this.state.value <= this.state.min }, "Less"),
+            h("span", { class: "value" }, this.state.value),
+            h("button", { onClick: () => this.increment(), disabled: this.state.value >= this.state.max }, "More")));
+    }
     static get is() { return "msrd-range-test"; }
     static get encapsulation() { return "shadow"; }
     static get properties() { return { "host": { "elementRef": true }, "max": { "type": Number, "attr": "max", "watchCallbacks": ["watchMax"] }, "min": { "type": Number, "attr": "min", "watchCallbacks": ["watchMin"] }, "state": { "state": true }, "value": { "type": Number, "attr": "value", "watchCallbacks": ["watchValue"] } }; }
-    static get events() { return [{ "name": "change", "method": "change", "bubbles": true, "cancelable": true, "composed": true }]; }
+    static get events() { return [{ "name": "change", "method": "change", "bubbles": true, "cancelable": true, "composed": true }, { "name": "lifeCycle", "method": "lifeCycle", "bubbles": true, "cancelable": true, "composed": true }]; }
     static get style() { return "/**style-placeholder:msrd-range-test:**/"; }
 }
--- src/components/range-test/range-test.tsx
+++ src/components/range-test/range-test.tsx
@@ -6,46 +6,54 @@ import { Component, Element, Event, EventEmitter, Prop, State, Watch } from '@st
   shadow: true
 })
 export class RangeTest {
-  @Prop() public value: number;
-  @Prop() public min: number;
-  @Prop() public max: number;
-
-  @Watch('value') public watchValue(value: number) { this.changeValue(value); }
-  @Watch('min') public watchMin(min: number) { this.changeMin(min); }
-  @Watch('max') public watchMax(max: number) { this.changeMax(max); }
-
-  @Event() public change: EventEmitter<number>;
-
   @Element() public host: HTMLElement;
 
   @State() public state: State = { value: 1, min: 0, max: 5 };
 
+  @Prop() public value: number;
+  @Watch('value')
+  public watchValue(value: number) {
+    this.changeValue(value);
+  }
+
+  @Prop() public min: number;
+  @Watch('min')
+  public watchMin(min: number) {
+    this.changeMin(min);
+  }
+
+  @Prop() public max: number;
+  @Watch('max')
+  public watchMax(max: number) {
+    this.changeMax(max);
+  }
+
+  @Event() public change: EventEmitter<number>;
+  @Event() public lifeCycle: EventEmitter<string>;
+
   public componentWillLoad() {
-    this.host.addEventListener('change', (event) => console.info('[wc:range]', 'emit:change', event['detail']));
+    console.log('[wc:range]', 'The component is about to be rendered');
+    this.lifeCycle.emit('willLoad');
   }
 
-  public render() {
-    return (
-      <div>
-        <button onClick={() => this.decrement()} disabled={this.state.value <= this.state.min}>
-          Less
-        </button>
-        <span class="value">
-          { this.state.value }
-        </span>
-        <button onClick={() => this.increment()} disabled={this.state.value >= this.state.max}>
-          More
-        </button>
-      </div>
-    );
+  public componentDidLoad() {
+    console.log('[wc:range]', 'The component has been rendered');
+    this.lifeCycle.emit('didLoad');
   }
 
-  private changeState(state: State, notify: boolean = false) {
-    console.info('[wc:range]', 'state change', this.state, state);
-    this.state = state;
-    if (notify) {
-      this.change.emit(state.value);
-    }
+  public componentWillUpdate() {
+    console.log('[wc:range]', 'The component will update');
+    this.lifeCycle.emit('willUpdate');
+  }
+
+  public componentDidUpdate() {
+    console.log('[wc:range]', 'The component did update');
+    this.lifeCycle.emit('didUpdate');
+  }
+
+  public componentDidUnload() {
+    console.log('[wc:range]', 'The view has been removed from the DOM');
+    this.lifeCycle.emit('didUnload');
   }
 
   private changeValue(value: number, notify: boolean = false): void {
@@ -77,12 +85,37 @@ export class RangeTest {
     }
   }
 
+  private changeState(state: State, notify: boolean = false) {
+    console.info('[wc:range]', 'state change', this.state, state);
+    this.state = state;
+    if (notify) {
+      console.info('[wc:range]', 'emit change', state.value);
+      this.change.emit(state.value);
+    }
+  }
+
   private increment(): void {
     this.changeValue(this.state.value + 1, true);
   }
 
   private decrement(): void {
     this.changeValue(this.state.value - 1, true);
+  }
+
+  public render() {
+    return (
+      <div>
+        <button onClick={() => this.decrement()} disabled={this.state.value <= this.state.min}>
+          Less
+        </button>
+        <span class="value">
+          { this.state.value }
+        </span>
+        <button onClick={() => this.increment()} disabled={this.state.value >= this.state.max}>
+          More
+        </button>
+      </div>
+    );
   }
 }
 
